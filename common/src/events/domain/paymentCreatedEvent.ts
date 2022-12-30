2930992be23f687f531bf24652890ac5542a65ef
Duygu CELOR <dcelor@webnet.fr>
2022-12-30 17:45:23 +0100

    update

---
+++ common/src/events/domain/paymentCreatedEvent.ts
@@ -0,0 +1,11 @@
+import { OrderStatus } from "./orderStatus";
+import { Subjects } from "./subjects";
+
+export interface PaymentCreatedEvent {
+  subject: Subjects.PaymentCreated;
+  data: {
+    id: string;
+    orderId: string;
+    stripeId: string;
+  };
+}
